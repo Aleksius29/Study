4792949b3f710d85c420190ee00331889da80638
Alex Grey <o.ponijay@gmail.com>
2017-12-26 23:17:24 +0200

    commut 1

---
+++ Start.java
@@ -0,0 +1,10 @@
+package javap;
+
+public class Start {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 10) {
+            System.out.println("qwerty");
+            counter++;
+        }
+    }
