75e1b981f0401c5b2b04b02b4fb50e97a5e92381
Oleksii Ponizhai <o.ponijay@gmail.com>
2017-12-30 17:07:51 +0200

    commit 15

--- ProjectStart/src/Study_1_1/Study_1_1.java
+++ ProjectStart/src/Study_1_1/Study_1_1.java
@@ -2,6 +2,8 @@ package Study_1_1;
 
 public class Study_1_1 {
     public static void main(String[] args) {
-
+        float pi = 3.14f;
+        pi = pi + 2 * 3;
+        System.out.print(pi);
     }
 }
