19808b9b8eb3cc250bd0ab84619a6654ea836682
Aleksius <o.ponijay@gmail.com>
2017-12-30 17:31:40 +0200

    Delete Start_1_0.java

--- src/JavaCore/Start_1_0.java
+++
@@ -1,8 +0,0 @@
-package JavaCore;
-
-public class Start_1_0 {
-    public static void main(String[] args) {
-        int num = 2
-        if (num > 0 || num < 5) {
-            System.out.print("Test");
-}
